75ba1bfcbfe11dbe54606e795af02ebf32d57e20
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-07-05 19:01:28 +0300

    Update Processing.java

--- Processing.java
+++ Processing.java
@@ -1,38 +1,37 @@
 import java.util.*;
-import java.util.function.Consumer;
 
-public class Processing {
+public class Processing{
+    List <String> alchemy_girls = Arrays.asList("Лямбдадельта", "Бернкастель", "Ника");
+    List <String> flower_girls = Arrays.asList("Рика", "Сатоко", "Ханю");
+    List<Integer> fuel_tanks = new ArrayList<>();
     Working w = new Working();
 
-    public static void main(String[] args) {
-        Processing p = new Processing();
-        p.alchemyFuel((sum_fuel)-> System.out.println("Девочки сделали: " + sum_fuel + " баков топлива."));
-    }
-    //здесь девочки-алхимики перерабатывают цветочки -> топливо
-    //девочки-механики перерабатывают мусор -> механические детали
-
-    public void alchemyFuel (Consumer<Integer> callback) {
-        String[] alchemist = {"Лямбдадельта", "Бернкастель", "Ника"};
-        String[] flower_picker = {"Рика", "Сатоко", "Ханю"};
-        List <Integer> flower = new ArrayList<>();
-        w.flowerPicker((previous_flowers) -> flower.addAll(previous_flowers));
+    public Processing(){
         Timer timer = new Timer();
-        List<Integer> fuel_tanks = new ArrayList<>();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                int flower_sum = w.sum(flower);
-                int fuel = (flower_sum/ flower_picker.length) * alchemist.length;
-                fuel_tanks.add(fuel);
-                int sum_fuel = w.sum(fuel_tanks);
-                //в счетчике убрать переработанные цветочки
-                callback.accept(sum_fuel);
+                alchemyFuel(flower_girls);
+                mechanicDetails();
             }
         };
         timer.schedule(task, 0, 15000);
     }
 
-    public void mechanicDetails (){
+    public static void main(String[] args) {
+        Processing p = new Processing();
+    }
 
+    public int alchemyFuel(List<String> flower_girls){
+        int flower_sum = w.flowerPicker(flower_girls);
+        int fuel = (flower_sum/ flower_girls.size()) * alchemy_girls.size();
+        fuel_tanks.add(fuel);
+        System.out.println("Топлива: " + w.sum(fuel_tanks));
+        return w.sum(fuel_tanks);
+    }
+
+    public int mechanicDetails (){
+        int details = 0;
+        return details;
     }
 }
