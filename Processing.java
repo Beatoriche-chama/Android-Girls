21ff61e11e8c46283e5eff583f0b68412400666a
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-07-06 13:16:32 +0300

    Update Processing.java

--- Processing.java
+++ Processing.java
@@ -1,10 +1,14 @@
 import java.util.*;
 
 public class Processing{
-    List <String> alchemy_girls = Arrays.asList("Лямбдадельта", "Бернкастель", "Ника");
-    List <String> flower_girls = Arrays.asList("Рика", "Сатоко", "Ханю");
+    List <String> alchemy_girls = Arrays.asList("Nika", "Rachel", "Lily");
+    List <String> flower_girls = Arrays.asList("Anna", "Nina", "Alice");
+    List <String> garbager = Arrays.asList("Rika", "Elly", "Rina", "Kira");
+    List <String> mechanic = Arrays.asList("Rika", "Elly");
     List<Integer> fuel_tanks = new ArrayList<>();
+    List <Integer> details_stack = new ArrayList<>();
     Working w = new Working();
+
 
     public Processing(){
         Timer timer = new Timer();
@@ -12,7 +16,7 @@ public class Processing{
             @Override
             public void run() {
                 alchemyFuel(flower_girls);
-                mechanicDetails();
+                mechanicDetails(garbager);
             }
         };
         timer.schedule(task, 0, 15000);
@@ -23,15 +27,30 @@ public class Processing{
     }
 
     public int alchemyFuel(List<String> flower_girls){
-        int flower_sum = w.flowerPicker(flower_girls);
-        int fuel = (flower_sum/ flower_girls.size()) * alchemy_girls.size();
+        int flower_sum = w.sum(w.getFlowerList());
+        int fuel = (flower_sum / flower_girls.size()) * alchemy_girls.size();
         fuel_tanks.add(fuel);
-        System.out.println("Топлива: " + w.sum(fuel_tanks));
+        int now_flowers = flower_sum - fuel;
+        System.out.println("Алхимики расходовали " + flower_sum + " цветочков и их осталось " +
+                now_flowers);
+        w.flowers.clear();
+        w.flowers.add(now_flowers);
+        System.out.println("Алхимики сделали топлива: " + w.sum(fuel_tanks));
         return w.sum(fuel_tanks);
     }
 
-    public int mechanicDetails (){
-        int details = 0;
-        return details;
+    public int mechanicDetails (List<String> garbager){
+        int garbage_sum = w.sum(w.getGarbageList());
+        int details = (garbage_sum / garbager.size()) * mechanic.size();
+        details_stack.add(details);
+        int now_garbage = garbage_sum - details;
+        System.out.println("Механики переработали " + garbage_sum + " мусора и их осталось " +
+                now_garbage);
+        w.garbage.clear();
+        w.garbage.add(now_garbage);
+        System.out.println("Механики изготовили деталек: " + w.sum(details_stack));
+        return w.sum(details_stack);
     }
 }
+
+
