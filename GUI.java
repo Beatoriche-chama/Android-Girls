b1c9f015a7149b245317e3554b88f7d9f8ed5cfc
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-07-13 18:05:20 +0300

    Update GUI.java
    new game accepted

--- GUI.java
+++ GUI.java
@@ -12,15 +12,18 @@ public class GUI extends JFrame {
     Mechanic m = new Mechanic();
     FileManage fileManage = new FileManage();
     EnergyGenerator energyGenerator = new EnergyGenerator();
+    JPanel panel = new JPanel();
+    Android_Helper android_helper = new Android_Helper();
+    JLabel name = new JLabel();
+    JLabel energy_count = new JLabel("15 энергии сейчас");
 
     public static void main(String args[]) {
-        GUI gui = new GUI();
+        new GUI();
     }
 
 
     public GUI() {
         JFrame frame = new JFrame("Android Girls");
-        JPanel panel = new JPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
@@ -32,6 +35,103 @@ public class GUI extends JFrame {
                 " механического тела, собирайте вместе новых робо-девочек и стремитесь" +
                 " к робораю. ALL HAIL TECHNOCRACY!!");
 
+        start();
+        timerCheck();
+
+        JLabel flower_count = new JLabel("0 цветов");
+        panel.add(flower_count);
+        Dimension flower_count_size = flower_count.getPreferredSize();
+        flower_count.setBounds(800, 125, 200, flower_count_size.height);
+
+        JLabel garbage_count = new JLabel("0 мусора");
+        panel.add(garbage_count);
+        Dimension garbage_count_size = garbage_count.getPreferredSize();
+        garbage_count.setBounds(800, 200, 200, garbage_count_size.height);
+
+        JLabel fuel_count = new JLabel("0 топлива");
+        panel.add(fuel_count);
+        Dimension fuel_count_size = fuel_count.getPreferredSize();
+        fuel_count.setBounds(800, 275, 200, fuel_count_size.height);
+
+        JLabel details_count = new JLabel("0 деталек");
+        panel.add(details_count);
+        Dimension details_count_size = details_count.getPreferredSize();
+        details_count.setBounds(800, 350, 200, details_count_size.height);
+
+        ImageIcon icon1 = new ImageIcon("C:/Users/User/Downloads/Android_girls/src/button.jpg");
+        JButton pick_flower = new JButton("Pick flowers", icon1);
+        pick_flower.setFont(new Font("Century Schoolbook", Font.BOLD, 16));
+        pick_flower.setHorizontalTextPosition(SwingConstants.CENTER);
+        panel.add(pick_flower);
+        pick_flower.setBounds(600, 110, 145, 60);
+
+        TimerWrapper pick_flowers = new TimerWrapper(flower_count, 5_000);
+        pick_flower.addActionListener(v -> pick_flowers.run(() -> {
+            try {
+                return p.flowerPicker();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, " собрано цветочков (´｡• ω •｡`)"));
+
+        JButton pick_garbage = new JButton("Pick garbage", icon1);
+        pick_garbage.setFont(new Font("Century Schoolbook", Font.BOLD, 16));
+        pick_garbage.setHorizontalTextPosition(SwingConstants.CENTER);
+        panel.add(pick_garbage);
+        pick_garbage.setBounds(600, 180, 145, 60);
+
+        TimerWrapper pick_old_staff = new TimerWrapper(garbage_count, 5_000);
+        pick_garbage.addActionListener(k -> pick_old_staff.run(() -> {
+            try {
+                return g.garbagePicker();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, " собрано мусора (*¯︶¯*)"));
+
+        JButton make_fuel = new JButton("Extract fuel", icon1);
+        make_fuel.setFont(new Font("Century Schoolbook", Font.BOLD, 16));
+        make_fuel.setHorizontalTextPosition(SwingConstants.CENTER);
+        panel.add(make_fuel);
+        make_fuel.setBounds(600, 250, 145, 60);
+
+        TimerWrapper making_fuel = new TimerWrapper(fuel_count, 15_000);
+        make_fuel.addActionListener(v -> making_fuel.run(() -> {
+            try {
+                return a.alchemyFuel();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, " топлива выделено (≧◡≦)"));
+
+        JButton make_details = new JButton("Make details", icon1);
+        make_details.setFont(new Font("Century Schoolbook", Font.BOLD, 16));
+        make_details.setHorizontalTextPosition(SwingConstants.CENTER);
+        panel.add(make_details);
+        make_details.setBounds(600, 320, 145, 60);
+
+        TimerWrapper making_details = new TimerWrapper(fuel_count, 15_000);
+        make_details.addActionListener(v -> making_details.run(() -> {
+            try {
+                return m.mechanicDetails();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, " деталек изготовлено <(￣︶￣)>"));
+
+        JOptionPane.showMessageDialog(GUI.this, "Девочки-андроиды выключатся, " +
+                "если запас энергии будет на нуле. Соберите достаточно деталек, чтобы починить ветряной генератор. " +
+                "Пока что энергии есть некоторое количество, но поторопитесь, из-за поломки она сейчас " +
+                "не пополняется.");
+
+        frame.setVisible(true);
+    }
+
+    public void start() {
         try {
             fileManage.fileCreate("girls_count");
             fileManage.fileSave("girls_count", 1);
@@ -39,7 +139,7 @@ public class GUI extends JFrame {
             e.printStackTrace();
         }
 
-        Android_Helper android_helper = new Android_Helper();
+
         ImageIcon image = new ImageIcon("C:/Users/User/Downloads/Android_girls/src/image" +
                 android_helper.giveMeicon() + ".png");
         JLabel icon = new JLabel(image);
@@ -47,7 +147,7 @@ public class GUI extends JFrame {
         Dimension icon_size = icon.getPreferredSize();
         icon.setBounds(30, 50, icon_size.width, icon_size.height);
 
-        JLabel name = new JLabel(android_helper.giveMeName());
+        name.setText(android_helper.giveMeName());
         panel.add(name);
         Dimension name_size = name.getPreferredSize();
         name.setBounds(350, 90, name_size.width, name_size.height);
@@ -72,93 +172,9 @@ public class GUI extends JFrame {
             setVisible(true);
         });
 
-        JLabel flower_count = new JLabel("0 цветов");
-        panel.add(flower_count);
-        Dimension flower_count_size = flower_count.getPreferredSize();
-        flower_count.setBounds(800, 125, 200, flower_count_size.height);
 
-        JLabel garbage_count = new JLabel("0 мусора");
-        panel.add(garbage_count);
-        Dimension garbage_count_size = garbage_count.getPreferredSize();
-        garbage_count.setBounds(800, 200, 200, garbage_count_size.height);
-
-        JLabel fuel_count = new JLabel("0 топлива");
-        panel.add(fuel_count);
-        Dimension fuel_count_size = fuel_count.getPreferredSize();
-        fuel_count.setBounds(800, 275, 200, fuel_count_size.height);
-
-        JLabel details_count = new JLabel("0 деталек");
-        panel.add(details_count);
-        Dimension details_count_size = details_count.getPreferredSize();
-        details_count.setBounds(800, 350, 200, details_count_size.height);
-
-        JButton pick_flower = new JButton("Собрать цветы");
-        panel.add(pick_flower);
-        pick_flower.setBounds(600, 110, 130, 60);
-
-        TimerWrapper pick_flowers = new TimerWrapper(flower_count, 5_000);
-        pick_flower.addActionListener(v -> pick_flowers.run(() -> {
-            try {
-                return p.flowerPicker();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, " собрано цветочков (´｡• ω •｡`)"));
-
-        ImageIcon icon1 = new ImageIcon("C:/Users/User/Downloads/Android_girls/src/button.jpg");
-        JButton pick_garbage = new JButton("Pick garbage", icon1);
-        pick_garbage.setFont(new Font("Century Schoolbook", Font.BOLD, 16));
-        pick_garbage.setHorizontalTextPosition(SwingConstants.CENTER);
-        panel.add(pick_garbage);
-        pick_garbage.setBounds(600, 180, 145, 60);
-
-        TimerWrapper pick_old_staff = new TimerWrapper(garbage_count, 5_000);
-        pick_garbage.addActionListener(k -> pick_old_staff.run(() -> {
-            try {
-                return g.garbagePicker();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, " собрано мусора (*¯︶¯*)"));
-
-        JButton make_fuel = new JButton("Экстрактить топливо");
-        panel.add(make_fuel);
-        make_fuel.setBounds(600, 250, 160, 60);
-
-        TimerWrapper making_fuel = new TimerWrapper(fuel_count, 15_000);
-        make_fuel.addActionListener(v -> making_fuel.run(() -> {
-            try {
-                return a.alchemyFuel();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, " топлива выделено (≧◡≦)"));
-
-        JButton make_details = new JButton("Выплавить детальки");
-        panel.add(make_details);
-        make_details.setBounds(600, 320, 170, 60);
-
-        TimerWrapper making_details = new TimerWrapper(fuel_count, 15_000);
-        make_details.addActionListener(v -> making_details.run(() -> {
-            try {
-                return m.mechanicDetails();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, " деталек изготовлено <(￣︶￣)>"));
-
-        JOptionPane.showMessageDialog(GUI.this, "Девочки-андроиды выключатся, " +
-                "если запас энергии будет на нуле. Соберите достаточно деталек, чтобы починить ветряной генератор. " +
-                "Пока что энергии есть некоторое количество, но поторопитесь, из-за поломки она сейчас " +
-                "не пополняется.");
-        //ЗДЕСЬ ИГРА СТАРТУЕТ
-        JLabel energy_count = new JLabel();
         panel.add(energy_count);
-        Dimension energy_count_size = flower_count.getPreferredSize();
+        Dimension energy_count_size = energy_count.getPreferredSize();
         energy_count.setBounds(930, 125, 200, energy_count_size.height);
 
         fileManage.fileCreate("energy_count");
@@ -178,39 +194,49 @@ public class GUI extends JFrame {
             return null;
         }, " энергии сейчас (´-ω-`)");
 
+    }
+
+    public void timerCheck() {
         Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 try {
                     System.out.println("Проверяю, что не ноль.");
                     if (fileManage.fileLoad("energy_count") < 1) {
                         timer.cancel();
-                        System.out.println("Конец.");
-                        UIManager.put("OptionPane.yesButtonText", "Помочь мечте андроидов сбыться в новой реальности");
-                        UIManager.put("OptionPane.noButtonText", "Оставить этот умирающий мир");
-                        int result = JOptionPane.showConfirmDialog(GUI.this,
-                                "Няша, твоя рабыня навеки застыла без энергии." +
-                                        " Сейчас ее программное обеспечение связи с параллельным миром отключится.",
-                                "Конец реальности", JOptionPane.YES_NO_OPTION);
-                        if (result == JOptionPane.YES_OPTION) {
-                            //restart();
-                            //счетчик обновляется, создаются новые файлы андроида и энергии
-                        }
-                        if (result == JOptionPane.NO_OPTION) {
-                            System.exit(0);
-                        }
-
+                        timer.purge();
+                        System.out.println("Больше не проверяю.");
+                        badEndmessage();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }, 0, 5000);
-        //ЗДЕСЬ ЗАКАНЧИВАЕТСЯ
+        };
 
-        frame.setVisible(true);
+        timer.schedule(timerTask, 0, 5000);
     }
 
+    public void badEndmessage() throws IOException {
+        if (fileManage.fileLoad("energy_count") < 1) {
+            System.out.println("Конец игры.");
+            UIManager.put("OptionPane.yesButtonText", "Помочь мечте андроидов сбыться в новой реальности");
+            UIManager.put("OptionPane.noButtonText", "Оставить этот умирающий мир");
+            int result = JOptionPane.showConfirmDialog(GUI.this,
+                    "Няша, твоя рабыня навеки застыла без энергии." +
+                            " Сейчас ее программное обеспечение связи с параллельным миром отключится.",
+                    "Конец реальности", JOptionPane.YES_NO_OPTION);
+            if (result == JOptionPane.YES_OPTION) {
+                name.setText("");
+                energy_count.setText("15 энергии сейчас");
+                start();
+                timerCheck();
+            }
+            if (result == JOptionPane.NO_OPTION) {
+                System.exit(0);
+            }
 
+        }
+    }
 }
