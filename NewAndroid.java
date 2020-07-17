5c7c7ec26fca9faf6568699fcfdf6455504a1306
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-07-17 21:09:33 +0300

    Update NewAndroid.java

--- NewAndroid.java
+++ NewAndroid.java
@@ -1,26 +1,29 @@
+import java.io.IOException;
+
 public class NewAndroid {
     //здес делают новых андроидов-девочек
-
-   Mechanic mechanic = new Mechanic();
     Android_Helper android_helper = new Android_Helper();
-    GirlsList girlsList = new GirlsList();
+    FileManage fileManage = new FileManage();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
-    public void makingNewGirl(){
-        int details_sum = android_helper.sum(mechanic.getDetailsList());
+    public int makingNewGirl() throws IOException {
+        int girls_count = fileManage.fileLoad("all_girls_count");
+        int details_sum = fileManage.fileLoad("details_count");
         int newGirl = 10;
-        if (details_sum >=10) {
+        if (details_sum >= 10) {
             int now_details = details_sum - newGirl;
-            mechanic.details_stack.clear();
-            mechanic.details_stack.add(now_details);
+            fileManage.fileSave("details_count", now_details);
             String girl = android_helper.giveMeName();
             System.out.println("Появилась новая девочка~, ее зовут " + girl);
-            girlsList.all_girls.add(girl);
+            fileManage.girlSave("free_girls_names", girl);
+            girls_count += girls_count;
+            fileManage.fileSave("all_girls_count", girls_count);
+        } else {
+            System.out.println("Не хватает деталек");
         }
-        else{System.out.println("Не хватает деталек");}
-
+        return girls_count;
     }
 }
