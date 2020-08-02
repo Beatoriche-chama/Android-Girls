f8f783a1ac1508b853436871568a2a4e9a37b7cc
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-08-02 16:10:47 +0300

    Update ManagementMenu.java

--- ManagementMenu.java
+++ ManagementMenu.java
@@ -1,30 +1,55 @@
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
-import javax.swing.*;
 
 
 public class ManagementMenu implements ActionListener, ItemListener {
-    FileManage fileManage = new FileManage();
-    DefaultListModel free_girls_model= new DefaultListModel();
+    GirlsLists girlsLists = GirlsLists.getInstance();
+    DefaultListModel free_girls_model = new DefaultListModel();
     DefaultListModel mechanic_girls_model = new DefaultListModel();
     DefaultListModel garbagers_model = new DefaultListModel();
     JList free_girls_list = new JList(free_girls_model);
     JList garbagers_list = new JList(garbagers_model);
     JList mechanics_list = new JList(mechanic_girls_model);
 
+    public void createPopUpMenu(JButton manage, JLabel free_label, JLabel garbagers_label, Thread garbage_thread,
+                                JLabel mechanic_label, Thread mechanic_thread){
+        JPopupMenu menu = new JPopupMenu("Menu");
+        JMenu hireSubmenu = new JMenu("Hire");
+        JMenu hireGarbargers = new JMenu("to Garbagers");
+        JMenuItem toWastelands = new JMenuItem("In wastelands");
+        toWastelands.addActionListener(e -> {
+            hireGirl("garbage_picker", garbagers_label, free_label,
+                    "Garbagers: ", garbage_thread);
+
+        });
+        JMenuItem toGreenhouse = new JMenuItem("In greenhouse");
+        //если нет теплицы, то функция не работает
+        toGreenhouse.setEnabled(false);
+        hireGarbargers.add(toWastelands);
+        hireGarbargers.add(toGreenhouse);
+        JMenu hireMechanics = new JMenu("to Mechanics");
+        JMenuItem make_details = new JMenuItem("Making details");
+        make_details.addActionListener(e -> {
+            hireGirl("mechanic_gurlz", mechanic_label, free_label,
+                    "Mechanics: ", mechanic_thread);
+
+        });
+        hireSubmenu.add(hireGarbargers);
+        hireSubmenu.add(hireMechanics);
+        JMenu dismissSubmenu = new JMenu("Dismiss");
+        JMenuItem dismissGarbargers = new JMenuItem("from Garbagers");
+        JMenuItem dismissMechanics = new JMenuItem("from Mechanics");
+        dismissSubmenu.add(dismissGarbargers);
+        dismissSubmenu.add(dismissMechanics);
+        menu.add(hireSubmenu);
+        menu.add(dismissSubmenu);
+        menu.show(manage, manage.getWidth() / 2, manage.getHeight() / 2);
+    }
 
     public JMenuBar createMenuBar() {
-        //create a menubar
 
         JMenuBar menuBar;
         JMenu managemenu;
@@ -51,15 +76,7 @@ public class ManagementMenu implements ActionListener, ItemListener {
         garbage_menuItem.getAccessibleContext().setAccessibleDescription("");
         garbage_menuItem.addActionListener(e -> {
 
-            String girl = null;
-            try {
-                girl = hireGirl("C:/Users/User/Documents/NyanData/garbagers_girls_count",
-                        "C:/Users/User/Documents/NyanData/garbagers_girls_names");
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-            checkListPlz("C:/Users/User/Documents/NyanData/garbagers_girls_names", garbagers_model);
-            free_girls_model.removeElement(girl);
+
 
         });
         hiresubmenu.add(garbage_menuItem);
@@ -67,15 +84,7 @@ public class ManagementMenu implements ActionListener, ItemListener {
         mechanic_menuItem = new JMenuItem("Mechanics");
         mechanic_menuItem.getAccessibleContext().setAccessibleDescription("");
         mechanic_menuItem.addActionListener(k -> {
-            String girl = null;
-            try {
-                girl = hireGirl("C:/Users/User/Documents/NyanData/mechanic_girls_count",
-                        "C:/Users/User/Documents/NyanData/mechanic_girls_names");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            checkListPlz("mechanic_girls_names", mechanic_girls_model);
-            free_girls_model.removeElement(girl);
+
         });
         hiresubmenu.add(mechanic_menuItem);
 
@@ -87,22 +96,23 @@ public class ManagementMenu implements ActionListener, ItemListener {
         garbage1_menuItem = new JMenuItem("Garbargers");
         garbage1_menuItem.getAccessibleContext().setAccessibleDescription("");
         garbage1_menuItem.addActionListener(b -> {
-            String girl = null;
+            /*String girl = null;
             try {
                 girl = dismissGirl("C:/Users/User/Documents/NyanData/garbagers_girls_count",
                         "C:/Users/User/Documents/NyanData/garbagers_girls_names");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            checkListPlz("C:/Users/User/Documents/NyanData/free_girls_names", free_girls_model);
+            checkListPlz((ArrayList<String>) girlsLists.free_girls, free_girls_model);
             garbagers_model.removeElement(girl);
+             */
         });
         dismisssubmenu.add(garbage1_menuItem);
 
         mechanic1_menuItem = new JMenuItem("Mechanics");
         mechanic1_menuItem.getAccessibleContext().setAccessibleDescription("");
         mechanic1_menuItem.addActionListener(l -> {
-            String girl = null;
+            /*String girl = null;
             try {
                 girl = dismissGirl("C:/Users/User/Documents/NyanData/mechanic_girls_count",
                         "C:/Users/User/Documents/NyanData/mechanic_girls_names");
@@ -111,6 +121,7 @@ public class ManagementMenu implements ActionListener, ItemListener {
             }
             checkListPlz("C:/Users/User/Documents/NyanData/free_girls_names", free_girls_model);
             mechanic_girls_model.removeElement(girl);
+             */
         });
         dismisssubmenu.add(mechanic1_menuItem);
 
@@ -118,9 +129,12 @@ public class ManagementMenu implements ActionListener, ItemListener {
     }
 
     public void createManageMenu() {
-        checkListPlz("C:/Users/User/Documents/NyanData/free_girls_names", free_girls_model);
-        checkListPlz("C:/Users/User/Documents/NyanData/garbagers_girls_names", garbagers_model);
-        checkListPlz("C:/Users/User/Documents/NyanData/mechanic_girls_names", mechanic_girls_model);
+        System.out.println("Безработных: " + girlsLists.free_girls.size());
+        System.out.println("Старьевщиц: " + girlsLists.garbage_picker.size());
+        System.out.println("Механиков: " + girlsLists.mechanic_gurlz.size());
+        checkListPlz(girlsLists.free_girls, free_girls_model);
+        checkListPlz(girlsLists.garbage_picker, garbagers_model);
+        checkListPlz(girlsLists.mechanic_gurlz, mechanic_girls_model);
         JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
         JPanel pan1 = new JPanel(new GridLayout(3, 1));
         pan1.add(free_girls_list);
@@ -144,59 +158,49 @@ public class ManagementMenu implements ActionListener, ItemListener {
         frame.setVisible(true);
     }
 
-    private String hireGirl(String job_counter, String job_name) throws IOException {
-        String free_names = "C:/Users/User/Documents/NyanData/free_girls_names";
-        String random_girl_name = randomName(free_names);
-        int free_girls_count = fileManage.fileLoad("C:/Users/User/Documents/NyanData/free_girls_count");
+    private NewAndroid hireGirl(String job_name, JLabel worker_label, JLabel free_label, String worker,
+                            Thread thread) {
+        NewAndroid random_girl = randomName(girlsLists.free_girls);
+        int free_girls_count = girlsLists.getGirlsList("free_girls").size();
         if (free_girls_count > 0) {
-            fileManage.fileSave("C:/Users/User/Documents/NyanData/free_girls_count",
-                    free_girls_count - 1);
-            fileManage.fileSave(job_counter,
-                    1 + fileManage.fileLoad(job_counter));
-            fileManage.girlSave(job_name, random_girl_name);
-            fileManage.girlDelete(free_names, random_girl_name);
-        }
-        return random_girl_name;
-    }
-
-    private String dismissGirl(String job_counter, String job_name) throws IOException {
-        String random_girl_name = randomName(job_name);
-        int job_count = fileManage.fileLoad(job_counter);
-        if (job_count > 0) {
-            fileManage.fileSave(job_counter,
-                    job_count - 1);
-            fileManage.fileSave("C:/Users/User/Documents/NyanData/free_girls_count",
-                    1 + fileManage.fileLoad("C:/Users/User/Documents/NyanData/free_girls_count"));
-            fileManage.girlSave("C:/Users/User/Documents/NyanData/free_girls_names", random_girl_name);
-            fileManage.girlDelete(job_name, random_girl_name);
-        }
-        return random_girl_name;
-    }
-
-    private String randomName(String file_name) {
-        Scanner s = null;
-        try {
-            s = new Scanner(new File(file_name));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ArrayList<String> list = new ArrayList<>();
-        while (s.hasNext()) {
-            list.add(s.next());
-        }
-        s.close();
-        Random rand = new Random();
-        String name = list.get(rand.nextInt(list.size()));
-        return name;
-    }
-
-    private void checkListPlz(String file_name, DefaultListModel model) {
-        try {
-            for (String gurlz : fileManage.girlsLoad(file_name).toArray(new String[0])) {
-                model.addElement(gurlz);
+            girlsLists.free_girls.remove(random_girl);
+            girlsLists.getGirlsList(job_name).add(random_girl);
+            worker_label.setText(worker + girlsLists.getGirlsList(job_name).size());
+            free_label.setText("Free: " + girlsLists.free_girls.size());
+            if(!thread.isAlive()){
+                thread.start();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        return random_girl;
+    }
+
+    private NewAndroid dismissGirl(String job_name, JLabel worker_label, JLabel free_label, String worker,
+                               Thread thread){
+        NewAndroid random_girl = randomName(girlsLists.getGirlsList(job_name));
+        int job_count = girlsLists.getGirlsList(job_name).size();
+        if (job_count > 0) {
+            girlsLists.free_girls.remove(random_girl);
+            girlsLists.getGirlsList(job_name).remove(random_girl);
+            int size = girlsLists.getGirlsList(job_name).size();
+            worker_label.setText(worker + size);
+            free_label.setText("Free: " + girlsLists.free_girls.size());
+            if (size < 1){
+                thread.interrupt();
+            }
+        }
+        return random_girl;
+    }
+
+    private NewAndroid randomName(ArrayList<NewAndroid> list) {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+
+    private void checkListPlz(ArrayList <NewAndroid> list, DefaultListModel model) {
+        for (NewAndroid gurlz : list) {
+            model.addElement(gurlz.getName());
+            model.addElement(gurlz.getInfo());
+            model.addElement(gurlz.getVersion());
         }
     }
 
