cf42b7cd8c38c0e08cd80c512a23f8329aa948fe
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-08-07 15:35:16 +0300

    Update TimerWrapper.java

--- TimerWrapper.java
+++ TimerWrapper.java
@@ -4,31 +4,52 @@ import java.util.TimerTask;
 import java.util.function.Supplier;
 
 public class TimerWrapper {
-
     private final long period;
     private final JLabel item;
     private boolean isActivated;
+    private Supplier<Integer> supplier;
+    private final String cute;
+    private boolean isLimited;
+    private Timer timer;
 
-    public TimerWrapper(JLabel item, long period) {
+    public TimerWrapper(JLabel item, long period, Supplier<Integer> supplier, String cute, boolean isLimited) {
         this.isActivated = false;
         this.item = item;
         this.period = period;
+        this.supplier = supplier;
+        this.cute = cute;
+        this.isLimited = isLimited;
+        this.timer = new Timer();
     }
 
-    public void run(Supplier<Integer> supplier, String cute, boolean isLimited) {
-        if (isActivated) return;
+    public boolean getStatus() {
+        return isActivated;
+    }
+
+    public void addTask() {
+        if (!isActivated) return;
+        int x = supplier.get();
+        System.out.println("Сейчас при значении: " + x + " статус " + getStatus());
+        item.setText(x + cute);
+        if (isLimited && x < 1) {
+            timerStop();
+        }
+    }
+
+    public void timerStop() {
+        this.timer.cancel();
+        isActivated = false;
+    }
+
+    public void timerStart() {
+        this.timer = new Timer();
         isActivated = true;
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        this.timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                int x = supplier.get();
-                item.setText(x + cute);
-                if(isLimited && x < 1){
-                    timer.cancel();
-                }
+                addTask();
             }
-        }, 0, period);
+        },0, period);
     }
 
 }
