a22560941136c4cefc2f717d070aa1d09fd20326
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-08-28 02:05:55 +0300

    Update FileManage.java

--- FileManage.java
+++ FileManage.java
@@ -1,102 +1,30 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+mport java.io.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class FileManage {
 
+    public static void main (String [] args) throws IOException, ClassNotFoundException {
 
-    public String fileLoad(String file_name, String category, int certainLine, boolean isNumber) throws IOException {
-        int counter = 0;
-        String data = null;
-        String line;
-        if(isNumber){
-            data = "0";
-        }
-        if (Files.exists(Paths.get(file_name))) {
-            BufferedReader reader = Files.newBufferedReader(Paths.get(file_name));
-            try {
-                while ((line = reader.readLine()) != null) {
-                    counter++;
-                    if (counter == certainLine) {
-                        String[] new_line = line.split(category + "|;");
-                        data = new_line[1];
-                        reader.close();
-                        break;
-                    }
-                }
-
-            } catch (Exception e) {
-                System.out.println("Нужной строчки в файле нет!");
-            }
-        } else {
-            System.out.println("Нужного файла нет.");
-        }
-
-        return data;
-    }
+        FileManage fileManage = new FileManage();
+        ArrayList<NewAndroid> oldGirls = new ArrayList<>();
+        NewAndroid helper = new NewAndroid(true);
+        oldGirls.add(helper);
+        NewAndroid anotherGirl = new NewAndroid(false);
+        oldGirls.add(anotherGirl);
+        System.out.println(helper.getInfo());
+        System.out.println(anotherGirl.getInfo());
+        fileManage.objectsSave("C:/Users/User/Downloads/Meow/NewFile", oldGirls);
+        ArrayList<NewAndroid> newGirls;
+        newGirls = (ArrayList<NewAndroid>)(Object) fileManage.objectsLoad(
+                "C:/Users/User/Downloads/Meow/NewFile");
+        System.out.println(newGirls.get(0).getInfo());
+        System.out.println(newGirls.get(1).getInfo());
 
 
-    public File fileCreate(String file_name) {
-        File new_file = new File(file_name);
-        System.out.println("Создан новый файл по имени " + file_name);
-        return new_file;
-    }
 
-    public void fileSave(String file_name, String category, String data, int certainLine,
-                         boolean isNumber) throws IOException {
-        File temp = new File(file_name + "Temp");
-        Path sourceFilePath = Paths.get(temp.getAbsolutePath());
-        Path targetFilePath = Paths.get(file_name);
-        PrintWriter rewriter = new PrintWriter(Files.newBufferedWriter(sourceFilePath));
-        BufferedReader reader = Files.newBufferedReader(targetFilePath);
-        String line;
-        int counter = 0;
-        while ((line = reader.readLine()) != null || counter < certainLine) {
-            counter++;
-            if (counter == certainLine) {
-                if (isNumber) {
-                    line = category + Integer.parseInt(data) + ";";
-                } else {
-                    line = category + data + ";";
-                }
-            }
-            rewriter.println(line);
-        }
-        rewriter.close();
-        reader.close();
-        System.out.println("Удаление с этой линией " + certainLine);
-        Files.delete(targetFilePath);
-        Files.move(sourceFilePath, targetFilePath);
-    }
-
-    public void listSave(String file_name, ArrayList <String> list) throws IOException {
-        new File(file_name);
-        PrintWriter pw = new PrintWriter(Files.newBufferedWriter(Paths.get(file_name)));
-        for (String girls : list)
-            pw.println(girls);
-        pw.close();
-    }
-
-    public List<String> listLoad(String file_name) {
-        List<String> list = null;
-        if (Files.exists(Paths.get(file_name))) {
-            Scanner s = null;
-            try {
-                s = new Scanner(new File(file_name));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            list = new ArrayList<>();
-            while (s.hasNext()) {
-                list.add(s.next());
-            }
-            s.close();
-        }
-        return list;
     }
 
     public void deleteDirectory(File directory) {
@@ -113,6 +41,58 @@ public class FileManage {
             }
         }
         directory.delete();
+    }
+
+    public void objectsSave(String fileName, ArrayList<?> list) throws IOException {
+        File f = new File(fileName);
+        FileOutputStream fos = new FileOutputStream(f);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        for (Object s : list) {
+            oos.writeObject(s);
+            oos.flush();
+        }
+        oos.close();
+        fos.close();
+    }
+
+    public ArrayList<Object> objectsLoad(String fileName) throws IOException, ClassNotFoundException {
+        File f = new File(fileName);
+        FileInputStream fis = new FileInputStream(f);
+        ObjectInputStream input = new ObjectInputStream(fis);
+        ArrayList <Object> objects = new ArrayList<>();
+        boolean cont = true;
+        while (cont) {
+            try {
+                Object obj = input.readObject();
+                if (obj != null) {
+                    System.out.println(obj);
+                    objects.add(obj);
+                } else {
+                    cont = false;
+                }
+            }catch (EOFException e){
+                System.out.println("End of file reached");
+                break;
+            }
+        }
+        input.close();
+        fis.close();
+        return objects;
+    }
+
+    public void mapSave(String fileName, Map<?, ?> map) throws IOException {
+        Properties properties = new Properties();
+        properties.putAll(map);
+        FileOutputStream fos = new FileOutputStream(fileName);
+        properties.store(fos, null);
+        fos.close();
+    }
+
+    public Map<Object, Object> mapLoad(String fileName) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream(fileName);
+        properties.load(fis);
+        return new HashMap<>(properties);
     }
 
 }
